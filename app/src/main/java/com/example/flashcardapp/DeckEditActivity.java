2378f7aa7c20ce0dc94b0913ed49483cf6eaa595
thstein7 <31679672+thstein7@users.noreply.github.com>
2019-02-15 14:07:25 -0500

    Setup first activities

---
+++ app/src/main/java/com/example/flashcardapp/DeckEditActivity.java
@@ -0,0 +1,13 @@
+package com.example.flashcardapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeckEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deck_edit_activitiy);
+    }
+}
--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.flashcardapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button deckViewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        deckViewButton = (Button) findViewById(R.id.LaunchDeckButton);
+        deckViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DeckEditActivity.class));
+            }
+        });
     }
+
+
 }
