06cd4499f01b835073e3fa87eba1967d44f99bfc
Owen Koehler <owenakoehler@gmail.com>
2019-02-20 13:57:44 -0500

    added lifestyle method logging for DeckEdit

--- app/src/main/java/com/example/flashcardapp/DeckEditFragment.java
+++ app/src/main/java/com/example/flashcardapp/DeckEditFragment.java
@@ -3,6 +3,7 @@ package com.example.flashcardapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +11,18 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class DeckEditFragment extends Fragment {
+    private static final String TAG = "DeckEditFragment";
     private Button saveChangesButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView(LayoutInflater, ViewGroup, Bundle) called");
         View v = inflater.inflate(R.layout.fragment_deck_edit, container, false);
         saveChangesButton = (Button) v.findViewById(R.id.save_changes_button);
         saveChangesButton.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +35,41 @@ public class DeckEditFragment extends Fragment {
             }
         });
         return v;
+    }
+
+
+
+    /*
+     * Overriding lifestyle methods for logging.
+     */
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
