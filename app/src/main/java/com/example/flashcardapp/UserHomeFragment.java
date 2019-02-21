b4c729752c7f7c13eb7d7c43576e0eb8e648d9da
thstein7 <31679672+thstein7@users.noreply.github.com>
2019-02-21 16:32:53 -0500

    Modified user home screen.

--- app/src/main/java/com/example/flashcardapp/UserHomeFragment.java
+++ app/src/main/java/com/example/flashcardapp/UserHomeFragment.java
@@ -1,15 +1,28 @@
 package com.example.flashcardapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class UserHomeFragment extends Fragment {
+    private Button myDecksButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_user_home, container, false);
+        myDecksButton = (Button) v.findViewById(R.id.my_decks_button);
+        myDecksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity() != null) {
+                    getActivity().startActivity(new Intent(getActivity(), DeckMenuActivity.class));
+                }
+            }
+        });
         return v;
     }
 }
