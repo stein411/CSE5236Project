5c965b6e9bcf67d321fe8f1891b6669710af7f6a
thstein7 <31679672+thstein7@users.noreply.github.com>
2019-02-22 14:33:19 -0500

    Removed searching (to be replaced with SearchView)

--- app/src/main/java/com/example/flashcardapp/UserHomeFragment.java
+++ app/src/main/java/com/example/flashcardapp/UserHomeFragment.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 
 public class UserHomeFragment extends Fragment {
     private Button myDecksButton;
-    private Button searchButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,13 +21,6 @@ public class UserHomeFragment extends Fragment {
                 if (getActivity() != null) {
                     getActivity().startActivity(new Intent(getActivity(), DeckMenuActivity.class));
                 }
-            }
-        });
-        searchButton = (Button) v.findViewById(R.id.search_decks_button);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().startActivity(new Intent(getActivity(), SearchDecksActivity.class));
             }
         });
         return v;
