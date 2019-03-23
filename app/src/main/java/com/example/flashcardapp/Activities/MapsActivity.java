287b5fd5880c1e3f28f7ef80ebff3158f25c3d00
thstein7 <31679672+thstein7@users.noreply.github.com>
2019-03-23 15:20:50 -0400

    Maps test

--- app/src/main/java/com/example/flashcardapp/Activities/MapsActivity.java
+++ app/src/main/java/com/example/flashcardapp/Activities/MapsActivity.java
@@ -1,57 +1,161 @@
 package com.example.flashcardapp.Activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.flashcardapp.R;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener {
 
     private GoogleMap mMap;
     private Button backButton;
+    private final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 100;
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 finish();
             }
         });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_FINE_LOCATION) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setMyLocation();
+            } else {
+                // Permission was denied. Display an error message.
+                Toast.makeText(getApplicationContext(), "Cannot display location at this time", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng columbus = new LatLng(40, -83);
-        mMap.addMarker(new MarkerOptions().position(columbus).title("Marker in Columbus"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(columbus));
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            setMyLocation();
+        } else {
+            // Request permission to access location
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+        addHeatMap();
+    }
+
+    private void addHeatMap() {
+        List<LatLng> list = new ArrayList<>();
+
+        // Populate with dummy data
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                list.add(new LatLng(40 + (i/10000.0), -83 + (i/10000.0)));
+            }
+        }
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                list.add(new LatLng(40 - (i/10000.0), -83 + (i/10000.0)));
+            }
+        }
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                list.add(new LatLng(40 + (i/10000.0), -83 - (i/10000.0)));
+            }
+        }
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                list.add(new LatLng(40 - (i/10000.0), -83 - (i/10000.0)));
+            }
+        }
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                list.add(new LatLng(40 - (i/10000.0), -83));
+            }
+        }
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                list.add(new LatLng(40 + (i/10000.0), -83));
+            }
+        }
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                list.add(new LatLng(40, -83 - (i/10000.0)));
+            }
+        }
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                list.add(new LatLng(40, -83 + (i/10000.0)));
+            }
+        }
+
+
+        mProvider = new HeatmapTileProvider.Builder().data(list).build();
+        mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
+
+    private void setMyLocation() {
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
     }
 }
