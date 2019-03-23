faa6a126f784b2d311ac3f8d77263e813cf079a9
thstein7 <31679672+thstein7@users.noreply.github.com>
2019-03-22 20:20:19 -0400

    May not push this

--- app/src/main/java/com/example/flashcardapp/Activities/MainEmptyActivity.java
+++ app/src/main/java/com/example/flashcardapp/Activities/MainEmptyActivity.java
@@ -1,26 +1,66 @@
 package com.example.flashcardapp.Activities;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.flashcardapp.LoginActivity;
+import com.example.flashcardapp.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainEmptyActivity extends AppCompatActivity {
+    private final int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent activityIntent;
+        //Intent activityIntent;
 
         //TODO replace with login token logic
         //if (Util.getToken() != null) {
         //activityIntent = new Intent(this, UserHomeActivity.class);
         //} else {
-        activityIntent = new Intent(this, LoginActivity.class);
+        //activityIntent = new Intent(this, LoginActivity.class);
         //}
+        // Configure Google Sign In
+        // TODO add google sign in support
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+        startActivityForResult(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build(), RC_SIGN_IN);
+        //startActivity(activityIntent);
+        //finish();
+    }
 
-        startActivity(activityIntent);
-        finish();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(new Intent(getApplicationContext(), UserHomeActivity.class));
+                finish();
+            } else {
+                Toast.makeText(getApplicationContext(), "Issue", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
